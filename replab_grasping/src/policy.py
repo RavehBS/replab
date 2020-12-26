conroller better naming, minor bug fix for policy, add average accuracy calculation

replab_grasping/src/policy.py
@@ -96,7 +96,9 @@ class Pinto2016(Policy):
     def __init__(self, model_path=None, heightmaps=False):
         self.net = PintoGuptaNet(depth=False, binned_output=True).cuda()
         self.net = nn.DataParallel(self.net).cuda()
-        self.net.load_state_dict(torch.load(model_path))
+        #self.net.load_state_dict(torch.load(model_path))
+        #Raveh&Omer: adding strict = false
+        self.net.load_state_dict(torch.load(model_path), strict=False)
         self.net.eval()
         self.resize = make_resize_rgb(227, 227)
 

replab_grasping/training/train.py
@@ -206,7 +206,8 @@ def main(batch_size, epochs, lr, ignore_grasp, crop_radius, method,
 			resultpath + '/optimizer-epoch-%d.th' % (start - 1)))
     else:
         optimizer = optim.Adam(model.parameters(), lr=current_lr)
-
+	
+    sumValAcc = 0
     for epoch in range(start, epochs + 1):
         if epoch % 50 == 0:
             current_lr /= 10
@@ -221,8 +222,10 @@ def main(batch_size, epochs, lr, ignore_grasp, crop_radius, method,
                    'optimizer-epoch-%d.th' % epoch)
         torch.save(model.state_dict(), resultpath +
                    'model-epoch-%d.th' % epoch)
-
+	sumValAcc = sumValAcc + val_acc*100
+	
     writer.close()
+    print ('Average val acc: {}%'.format(sumValAcc/(epochs+1)))
     print('Done')
 
 